c077e1df543a20917aa36a4fb6d2d61391bdd8fb
jefnilham <39832806+jefnilham@users.noreply.github.com>
2023-08-27 21:26:18 -0500

    to replace old files

--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
+// When the 'extractButton' is clicked, perform the following actions
 document.getElementById('extractButton').addEventListener('click', function() {
+    // Query the currently active tab in the Chrome browser
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         const activeTab = tabs[0];
         chrome.scripting.executeScript({
@@ -6,7 +8,13 @@ document.getElementById('extractButton').addEventListener('click', function() {
             function: extractText
         }, function(results) {
             const extractedText = results[0].result;
-            extractIpAddress(extractedText);
+            const uniqueIpAddresses = extractIpAddress(extractedText);
+            displayUniqueIpAddresses(uniqueIpAddresses); // Display unique IP addresses in popup
+            console.log(uniqueIpAddresses); // Log unique IP addresses to console
+            // Initiate the download action when button is clicked
+            document.getElementById('extractButton').addEventListener('click', function() {
+                downloadUniqueIpAddresses(uniqueIpAddresses);
+            });
         });
     });
 });
@@ -21,16 +29,36 @@ function extractIpAddress(text) {
     const ipAddresses = text.match(ipAddressPattern);
 
     if (ipAddresses) {
-        const uniqueIpAddresses = [...new Set(ipAddresses)]; // Remove duplicates
-        const extractedTextPre = document.getElementById('extractedText');
-        
-        // Create hyperlinks for each unique IP address
-        const hyperlinks = uniqueIpAddresses.map(ip => {
-            const shodanUrl = `https://www.shodan.io/host/${ip}`;
-            return `<a href="${shodanUrl}" target="_blank">${ip}</a>`;
-        });
-
-        extractedTextPre.innerHTML = hyperlinks.join('<br>');
+        const uniqueIpAddresses = [...new Set(ipAddresses)];
+        return uniqueIpAddresses;
+    } else {
+        return [];
     }
 }
 
+function displayUniqueIpAddresses(uniqueIpAddresses) {
+    const extractedTextPre = document.getElementById('extractedText');
+    
+    // Create hyperlinks for each unique IP address
+    const hyperlinks = uniqueIpAddresses.map(ip => {
+        const shodanUrl = `https://www.shodan.io/host/${ip}`;
+        return `<a href="${shodanUrl}" target="_blank">${ip}</a>`;
+    });
+
+    extractedTextPre.innerHTML = hyperlinks.join('<br>');
+}
+
+function downloadUniqueIpAddresses(uniqueIpAddresses) {
+    const ipAddressesText = uniqueIpAddresses.join('\n');
+    const blob = new Blob([ipAddressesText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'unique_ip_addresses.txt';
+    document.body.appendChild(a);
+    a.click();
+    
+    URL.revokeObjectURL(url);
+}
